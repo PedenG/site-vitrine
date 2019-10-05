2a346e0098703ffa63061c8f5653205ff616586f
PedenG <glenpedenn@gmail.com>
2019-10-05 12:56:10 +0200

    correction du sitemap + settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(59zcy482ue)&f7utv*n7muqm8g#$&5sux^om^*4k_@*i^8qkk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['glen-peden.fr', 'www.glen-peden.fr', '127.0.0.1', '51.68.123.188']
 
