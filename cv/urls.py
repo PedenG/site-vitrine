ed3d30d8fdafa6347cecd52c63f6777a618074b7
PedenG <glenpedenn@gmail.com>
2020-02-11 16:44:58 +0100

    Modification des url canonique + compétences

--- cv/urls.py
+++ cv/urls.py
@@ -5,7 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.mainpage, name='mainpage'),
-    path('accueil', views.mainpage, name='accueil'),
     path('about', views.about, name='about'),
     path('contact', views.send_email, name='contact'),
     path('robots.txt', TemplateView.as_view(template_name="cv/robots.txt", content_type='text/plain')),
--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(59zcy482ue)&f7utv*n7muqm8g#$&5sux^om^*4k_@*i^8qkk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['glen-peden.fr', 'www.glen-peden.fr', '127.0.0.1', '51.68.123.188']
+ALLOWED_HOSTS = ['www.glen-peden.fr', '127.0.0.1', '51.68.123.188']
 
 
 # Application definition
